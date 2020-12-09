ca22f082301a80717f6e763d2719a3df187badff
Dmitriy <svsdima1995@mail.ru>
2020-12-09 12:52:26 +0300

    Создаем методы для работы с элементами, часть 2

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,9 @@ $('div').click(function() {
     console.log($(this).index());
 });
 
-console.log($('div').eq(2).find('.more'));
-console.log($('div').eq(2).find('.some'));
-console.log($('button').html());
+// console.log($('div').eq(2).find('.more'));
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme'));
+console.log($('.more').eq(0).siblings());
+
+// console.log($('button').html());
