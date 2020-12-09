7dacced93c91fe6536fba97ed6782cdd336172ce
Dmitriy <svsdima1995@mail.ru>
2020-12-09 13:48:29 +0300

    Работа со стилями + создаем готовые компоненты кнопок для библиотеки

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,16 @@
 import './lib/lib';
 import $ from './lib/lib';
 
-$('button').on('click', function() {
-    $('div').eq(1).toggleClass('active');
+$('#first').on('click', () => {
+    $('div').eq(1).fadeOut(800);
 });
 
-$('div').click(function() {
-    console.log($(this).index());
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeOut(800);
 });
 
-// console.log($('div').eq(2).find('.more'));
-// console.log($('div').eq(2).find('.some'));
-// console.log($('.some').closest('.findme'));
-// console.log($('.more').eq(0).siblings());
-$('.findme').fadeOut(1800);
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeOut(800);
+});
 
 // console.log($('button').html());
